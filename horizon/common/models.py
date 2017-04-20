37ca054c7021f82a8424fd1d2029d86914e8329e
Glauber Batista <glauberrbatista@gmail.com>
2017-04-20 11:41:50 -0300

    Fix auth model

--- horizon/common/models.py
+++ horizon/common/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 #from django.contrib.auth.models import User
 from django.conf import settings
+from openstack_auth.user import User
 
 
 class ExternalProfile(models.Model):
-    #user = models.OneToOneField(User)
-    user = settings.AUTH_USER_MODEL
+    user = models.OneToOneField(User)
+    #user = settings.AUTH_USER_MODEL
     external_id = models.CharField(max_length=150)
     access_token = models.CharField(max_length=1024)
     password =  models.CharField(max_length=150)
